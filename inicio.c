6af8135462417d2e3d26ecd4967aaa93b704ad71
lucasgiovine <126298506+lucasgiovine@users.noreply.github.com>
2023-03-01 22:13:31 -0300

    Add files via upload

---
+++ inicio.c
@@ -0,0 +1,15 @@
+int main ()
+{
+    float base, altura, area;
+
+    printf("Digite a base do triangulo:");
+    scanf("%f", &base );
+
+    printf("Digite a altura do triangulo:");
+    scanf("%f", &altura );
+
+    area = (base * altura /2 );
+
+    printf("A area do triangulo e: %.2f", area );
+
+}
